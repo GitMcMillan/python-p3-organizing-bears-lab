fe667e177db77abfabae9cb8f6bf908cb7d19741
alexandermcmillan <mcmillancontact@protonmail.com>
2024-08-16 20:57:28 -0700

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "black", "calm", "True");
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 12, "M", "black", "calm", "True");
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 12, "M", "black", "calm", "True");
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 12, "M", "black", "calm", "True");
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 12, "M", "black", "calm", "True");
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 12, "M", "black", "calm", "True");
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 12, "M", "black", "calm", "True");
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (Null, 12, "M", "black", "calm", "True");
+
